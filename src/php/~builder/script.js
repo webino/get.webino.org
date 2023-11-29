ea2c5587532f75f01093d8adce1ad2517f1f2cbb
Peter Bačinský <peter@bacinsky.sk>
2023-11-29 20:43:37 +0100

    Updated script.js, added intl support

--- src/php/~builder/script.js
+++ src/php/~builder/script.js
@@ -13,6 +13,7 @@
             form.setCommand(form.php_memcached.checked, 'php-memcached');
             form.setCommand(form.php_sqlite.checked, 'php-sqlite');
             form.setCommand(form.php_openssl.checked, 'php-openssl');
+            form.setCommand(form.php_intl.checked, 'php-intl');
             form.setCommand(form.php_fpm.checked, 'php-fpm');
             form.setCommand(form.php_short_tags.checked, 'php-short-tags');
         }
