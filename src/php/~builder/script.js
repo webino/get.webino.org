9dad2ec1d8f83d548a6a52c9680ed56de74f3a31
Peter Bačinský <peter@bacinsky.sk>
2023-11-29 19:11:14 +0100

    Updated script.js, added sqlite support

--- src/php/~builder/script.js
+++ src/php/~builder/script.js
@@ -11,8 +11,9 @@
             form.setCommand(form.php_apache.checked, 'apache');
             form.setCommand(form.php_mysql.checked, 'mysql');
             form.setCommand(form.php_memcached.checked, 'php-memcached');
-            form.setCommand(form.php_fpm.checked, 'php-fpm');
+            form.setCommand(form.php_sqlite.checked, 'php-sqlite');
             form.setCommand(form.php_openssl.checked, 'php-openssl');
+            form.setCommand(form.php_fpm.checked, 'php-fpm');
             form.setCommand(form.php_short_tags.checked, 'php-short-tags');
         }
     };
